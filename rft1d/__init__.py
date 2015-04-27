1cc3e173a6c88011e0c4afdde39c33b21545ce3f
Todd Pataky <0todd0@gmail.com>
2015-04-27 16:36:39 +0900

    Initial version upload.

---
+++ rft1d/__init__.py
@@ -0,0 +1,13 @@
+
+__version__  = '0.1.1'   #(2015.04.26)
+
+import data, distributions, geom, prob, random
+
+randn1d      = random.randn1d
+multirandn1d = random.multirandn1d
+
+chi2         = distributions.chi2
+f            = distributions.f
+norm         = distributions.norm
+t            = distributions.t
+T2           = distributions.T2
