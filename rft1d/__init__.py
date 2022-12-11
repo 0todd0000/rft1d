850c03fc36f3acc5109d2fc22784dd26a19d64cb
Todd Pataky <todd@todds-imac-pro.lan>
2022-12-11 17:00:18 +0900

    Update __init__.py

--- rft1d/__init__.py
+++ rft1d/__init__.py
@@ -1,16 +1,5 @@
-'''
-rft1d was previously released as a separate package.
-It is now integrated with spm1d, and is currently nearly identical to the separately downloadable rft1d pacakge.
 
-Future plans include:
-
-- integrate rft1d examples and data into spm1d examples
-- merge the ./rft1d/geom.py functionality with the spm1d cluster functionality
-- maintain the separate rft1d as a standalone package for users who want light-weight RFT probability calculations
-'''
-
-
-__version__  = '0.1.4 spm1d'   #(2016.10.01)
+__version__  = '0.2.0'   # (2022.12.11)
 
 from . import data, distributions, geom, prob, random
 
