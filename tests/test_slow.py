80221790458f56358b083cd347e042d764a3f88c
Todd Pataky <0todd0@gmail.com>
2022-12-14 10:39:54 +0900

    Add SPM12b test cases

--- tests/test_slow.py
+++ tests/test_slow.py
@@ -1,7 +1,9 @@
 
+import os
 import pytest
+from scipy.io import loadmat
 import rft1d
-print(rft1d)
+
 
 
 @pytest.mark.slow
@@ -13,5 +15,15 @@ def test_myslow():
 def test_myfast():
 	assert 1==1
 
+
+def test_load_mat():
+	dir0            = os.path.dirname( __file__ )
+	fname           = os.path.join(dir0,  'data-spm12b', 'testcases_p_RF.mat')
+	M               = loadmat(fname)
+	params,expected = M['PARAMS'], M['RESULTS']
+	assert params.shape[0]   == 2268000
+	assert params.shape[1]   == 9
+	assert expected.shape[0] == 2268000
+	assert expected.shape[1] == 4
 
 
